811f7f5384da977bb83396d8429e25dcadbd8203
pyae sone <pyaesonehtun397@gmail.com>
2024-07-29 00:46:10 +0630

    reduce exercise

--- array-exe.js
+++ array-exe.js
@@ -6,33 +6,44 @@ const employees = [
     {id:5,name:'koko',sallery:850000,department:"Finance",DOB:"09/25/1991"},
     {id:6,name:'aungaung',sallery:880000,department:"Finance",DOB:"09/25/1981"},
     {id:7,name:'rose',sallery:880000,department:"Account",DOB:"09/25/1995"},
-    {id:8,name:'deny',sallery:980000,department:"IT Dept:",DOB:"09/25/1993"},
+    {id:8,name:'deny',sallery:980000,department:"IT",DOB:"09/25/1993"},
     {id:9,name:'jolly',sallery:800000,department:"Account",DOB:"09/25/1999"},
-
 ]
-// const overThirty = employees.filter(user=>new Date().getFullYear()-new Date(user.DOB).getFullYear()>30)
-// console.log(overThirty);
-// const under30 =employees.sort((a,b)=>new Date(a.DOB).getFullYear() - new Date(b.DOB).getFullYear() < 30)
-// console.log(under30);
-// const emplpyeeDetails = employees.map(employee=>{
-//     return `${name} works in the ${department} departments and earns ${sallery / 1000}k per year`
-// });
-// console.log(emplpyeeDetails);
-let averageSallery = employees.reduce((acc,{sallery})=>acc+perseInt(sallery),0) / employees.length
+//filter နဲ့ employees ထဲက လူတွေရဲ့အသက်ကိုစစ်ခြင်း
+let over30 = employees.filter(user=>new Date().getFullYear() - new Date(user.DOB).getFullYear() >30);
+console.log('over30 :>> ', over30);
+
+// sort စီခြင်း
+let sortsee = employees.sort((a,b)=>new Date(a.DOB).getFullYear() - new Date(b.DOB).getFullYear());
+console.log('sortsee :>> ', sortsee);
+
+let seesort = employees.sort((b,a)=>new Date(b.DOB).getFullYear() - new Date(a.DOB).getFullYear());
+console.log('seesort :>> ', seesort);
+// map method ကိုသုံးပြီး စစ်ခြင်း
+const employDatails = employees.map((user)=>{
+    return `${user.name} works in the ${user.department} since ${user.DOB} and his sallary is ${user.sallery /1000}k.`;
+})
+console.log('employDatails :>> ', employDatails);
+// department ထဲမှာ လူဘယ်နယောက်ရှိလဲစစ်ခြင်း
+const result = employees.reduce((acc,user)=>({...acc,[user.department]:acc[user.department] +1 || 1}),{});
+console.log('result :>> ', result);
+//employees ထဲက လူအားလုံးပေါင်းရဲ့လစာကိုစစ်ခြင်း
+let total = employees.reduce((acc,user)=>acc + parseInt(user.sallery),0);
+console.log('total :>> ', total);
+let total2 = employees.reduce((acc,{sallery})=>acc + parseInt(sallery),0);
+console.log('total2 :>> ', total2);
+let averageSallery= employees.reduce((acc,{sallery})=>acc+parseInt(sallery),0) / employees.length;
 console.log('averageSallery :>> ', averageSallery);
 
-const acc = {b:2,c:3};
-const result ={...acc,['a']:'a'}
-console.log(result);
-const acc2 = {b:2,c:3,a:100};
-const result2 ={...acc,['a']:'apple'};
-console.log('result2 :>> ', result2);
-
-let res = employees.reduce((acc,employee)=>({...acc,[employee.department]:acc[employee.department]+1 || 1}))
-console.log('res :>> ', res);
-
-const nameGroup =employees.reduce((acc,user)=>{
-    const key =user.department;
+// department ထဲက လူအရေတွက်ကို နာမည်နဲ့ စစ်ခြင်း
+const nameGroup = employees.reduce((acc,user)=>{
+    const key = user.department;
     const existValue = acc[key] || [];
     return {...acc,[key] : [...existValue,user.name]}
-})
+},{});
+console.log('nameGroup :>> ', nameGroup);
+// highest sallery ကို စစ်ခြင်း
+const highest = employees.reduce((highest,employee)=>{
+    return employee.sallery > highest.sallery ? employee : highest;
+});
+console.log('highest :>> ', highest);
